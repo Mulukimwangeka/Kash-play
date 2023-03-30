5b9b76eb89511383e83558dd8c136bc0f530919b
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-30 11:51:20 +0300

    popular cards rendered

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Popular from "./Popular";
 import Adventures from "./Adventures";
 import Action from "./Action";
@@ -7,60 +7,64 @@ import Puzzle from "./Puzzle";
 import Android from "./Android";
 import Strategy from "./Strategy";
 import Subscribed from "./Subscribed";
+import "./Styles/Home.css";
 
 //home component
 
 function Home() {
-    return (
-        <div className="App">
-        <h1>Home</h1>
-{/* display home page cards */}
-<div className="Popular-card">
-<Popular/>
-</div>
+  const carouselRef = useRef();
 
-<br></br>
-
-<div className="Subscribed-card">
-    <Subscribed/>
-</div>
-
-<br></br>
-<div className="Adventure-card">
-    <Adventures/>
-</div>
-
-<br></br>
-
-<div className="Action-card">
-    <Action/>
-</div>
-
-<br></br>
-
-<div className="Race-card">
-    <Race/>
-</div>
-
-<br></br>
-
-<div className="Android-card">
-    <Android/>
-</div>
-
-<br></br>
-<div className="Puzzle-card">
-    <Puzzle/>
-</div>
-
-<br></br>
-
-<div className="Strategy-card">
-    <Strategy/>
-</div>
-
-</div>
-    );
+  const handleCarouselScroll = () => {
+    if (
+      carouselRef.current.scrollLeft + carouselRef.current.offsetWidth >=
+      carouselRef.current.scrollWidth
+    ) {
+      carouselRef.current.scrollTo({ left: 0, behavior: "smooth" });
+    } else {
+      carouselRef.current.scrollBy({
+        left: carouselRef.current.offsetWidth,
+        behavior: "smooth",
+      });
     }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(handleCarouselScroll, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <div className="carousel">
+        <div className="Popular-card" ref={carouselRef}>
+          <Popular />
+        </div>
+      </div>
+      <div className="Subscribed-card">
+        <Subscribed />
+      </div>
+      <div className="Adventure-card">
+        <Adventures />
+      </div>
+      <div className="Action-card">
+        <Action />
+      </div>
+      <div className="Race-card">
+        <Race />
+      </div>
+      <div className="Android-card">
+        <Android />
+      </div>
+      <div className="Puzzle-card">
+        <Puzzle />
+      </div>
+      <div className="Strategy-card">
+        <Strategy />
+      </div>
+    </div>
+  );
+}
 
 export default Home;
--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,28 +1,64 @@
-import React from "react";
-import "./Styles/Popular.css"
-// popular component
+import React, { useState, useEffect } from "react";
+import "./Styles/Popular.css";
 
 function Card(props) {
-    return (
-      <div className="card">
-        <h2>{props.name}</h2>
-        <p>{props.details}</p>
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="card" onClick={handleCardClick}>
+      <h2>{props.name}</h2>
+      <img src={props.image} alt={props.name} />
+      <p>{props.details}</p>
+      {modalIsOpen && (
+        <div className="modal">
+          <div className="popup">
+            <h2>{props.name}</h2>
+            <img src={props.image} alt={props.name} />
+            <p>Details: {props.details}</p>
+            <p>Charges: {props.charges}</p>
+            <p>Registration details: {props.registration}</p>
+            <button onClick={handleCloseClick}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Popular() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Popular</h1>
+      <div className="card-container">
+        {games.map((game) => (
+          <Card
+            key={game.id}
+            name={game.name}
+            details={game.details}
+            charges={game.charges}
+            registration={game.registration}
+            image={game.image}
+          />
+        ))}
       </div>
-    );
-  }
-  
-  function Popular() {
-    return (
-      <div className="App">
-        <h1>Popular</h1>
-        <div className="card-container">
-          <Card name="Game 1" details="Details for Game 1" />
-          <Card name="Game 2" details="Details for Game 2" />
-          <Card name="Game 3" details="Details for Game 3" />
-        </div>
-        </div>
-        );
-    
-    }
+    </div>
+  );
+}
 
 export default Popular;
