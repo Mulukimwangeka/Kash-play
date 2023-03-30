b7579e4ea8589ca237a1576c6aefcbdb6485125f
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-30 10:24:24 +0300

    popular cards rendered

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,13 +1,28 @@
 import React from "react";
+import "./Styles/Popular.css"
 // popular component
 
-function Popular() {
+function Card(props) {
     return (
-        <div className="App">
-        <h1>Popular</h1>
-{/* display popular page cards */}
-        </div>
+      <div className="card">
+        <h2>{props.name}</h2>
+        <p>{props.details}</p>
+      </div>
     );
+  }
+  
+  function Popular() {
+    return (
+      <div className="App">
+        <h1>Popular</h1>
+        <div className="card-container">
+          <Card name="Game 1" details="Details for Game 1" />
+          <Card name="Game 2" details="Details for Game 2" />
+          <Card name="Game 3" details="Details for Game 3" />
+        </div>
+        </div>
+        );
+    
     }
 
 export default Popular;
