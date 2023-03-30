c3bae1f0950acdf3a4f5c5d387d1b7fce16df7c2
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-30 19:35:06 +0300

    pages updated

--- src/Components/Android.js
+++ src/Components/Android.js
@@ -1,15 +1,70 @@
-//android components
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import React from 'react';
-function Android
-() {
-    return (
-        <div className="App">
-        <h1>Android</h1>
-{/* display android page cards */}
+function Android() {
+  const [androidData, setAndroidData] = useState([]);
+  const [selectedAndroid, setSelectedAndroid] = useState(null);
 
-        </div>
-    );
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setAndroidData(data));
+  }, []);
+
+  const handleCardClick = android => {
+    setSelectedAndroid(android);
+  };
+
+  const handleCloseClick = () => {
+    setSelectedAndroid(null);
+  };
+
+  const handleAddToGamesClick = () => {
+    if (!selectedAndroid) return;
+
+    // Get the user's games from local storage or create a new array if it doesn't exist.
+    const games = JSON.parse(localStorage.getItem('games')) || [];
+
+    // Check if the selected android game is already in the user's games.
+    const alreadyInGames = games.some(game => game.id === selectedAndroid.id);
+
+    if (!alreadyInGames) {
+      // Add the selected android game to the user's games.
+      games.push(selectedAndroid);
+
+      // Update the user's games in local storage.
+      localStorage.setItem('games', JSON.stringify(games));
     }
+
+    setSelectedAndroid(null);
+  };
+
+  return (
+    <div>
+      <h1>Android Games</h1>
+      <div className="card-container">
+        {androidData.map((android, index) => (
+          <div className="card" key={index} onClick={() => handleCardClick(android)}>
+            <h2>{android.name}</h2>
+            <img src={android.image} alt={android.name} />
+            <p>{android.details}</p>
+            <button>Play</button>
+          </div>
+        ))}
+      </div>
+      {selectedAndroid && (
+        <div className="modal">
+          <div className="popup">
+            <h2>{selectedAndroid.name}</h2>
+            <img src={selectedAndroid.image} alt={selectedAndroid.name} />
+            <p>{selectedAndroid.details}</p>
+            <button onClick={handleAddToGamesClick}>Add to my games</button>
+            <button onClick={handleCloseClick}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Android;
--- src/Components/Strategy.js
+++ src/Components/Strategy.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Strategy() {
+  const [strategyData, setStrategyData] = useState([]);
+  const [selectedStrategy, setSelectedStrategy] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setStrategyData(data));
+  }, []);
+
+  const handleCardClick = strategy => {
+    setSelectedStrategy(strategy);
+  };
+
+  const handleCloseClick = () => {
+    setSelectedStrategy(null);
+  };
+
+  const handleAddToGamesClick = () => {
+    if (!selectedStrategy) return;
+
+    // Get the user's games from local storage or create a new array if it doesn't exist.
+    const games = JSON.parse(localStorage.getItem('games')) || [];
+
+    // Check if the selected strategy game is already in the user's games.
+    const alreadyInGames = games.some(game => game.id === selectedStrategy.id);
+
+    if (!alreadyInGames) {
+      // Add the selected strategy game to the user's games.
+      games.push(selectedStrategy);
+
+      // Update the user's games in local storage.
+      localStorage.setItem('games', JSON.stringify(games));
+    }
+
+    setSelectedStrategy(null);
+  };
+
   return (
     <div>
-    <h1>Strategy</h1>
+      <h1>Strategy Games</h1>
+      <div className="card-container">
+        {strategyData.map((strategy, index) => (
+          <div className="card" key={index} onClick={() => handleCardClick(strategy)}>
+            <h2>{strategy.name}</h2>
+            <img src={strategy.image} alt={strategy.name} />
+            <p>{strategy.details}</p>
+            <button>Play</button>
+          </div>
+        ))}
+      </div>
+      {selectedStrategy && (
+        <div className="modal">
+          <div className="popup">
+            <h2>{selectedStrategy.name}</h2>
+            <img src={selectedStrategy.image} alt={selectedStrategy.name} />
+            <p>{selectedStrategy.details}</p>
+            <button onClick={handleAddToGamesClick}>Add to my games</button>
+            <button onClick={handleCloseClick}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Strategy
+export default Strategy;
